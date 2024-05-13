f927c2a75ff775c9f4eaf428776074b4a41ed81b
Berkay KIZGIR <berkaykizgir@outlook.com>
2024-05-13 18:11:06 +0300

    overflowfixed

--- lib/src/home/home_screen.dart
+++ lib/src/home/home_screen.dart
@@ -30,109 +30,111 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       drawer: Drawer(
         child: SafeArea(
-          child: Column(
-            children: [
-              ExpansionTile(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                ExpansionTile(
+                    title: const Text(
+                      'IAM',
+                      style: drawerMainMenuTextStyle,
+                    ),
+                    children: [
+                      ListTile(
+                        title: const Text('Accounts'),
+                        leading: const Icon(Icons.account_circle),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Users'),
+                        leading: const Icon(Icons.people),
+                        onTap: () {},
+                      ),
+                    ]),
+                ExpansionTile(
+                    title: const Text(
+                      'CRM',
+                      style: drawerMainMenuTextStyle,
+                    ),
+                    children: [
+                      ListTile(
+                        title: const Text('Accounts'),
+                        leading: const Icon(Icons.account_circle),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('User'),
+                        leading: const Icon(Icons.people),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Accounts Managers'),
+                        leading: const Icon(Icons.manage_accounts),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Opportunities'),
+                        leading: const Icon(Icons.money),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Quotes'),
+                        leading: const Icon(Icons.request_quote),
+                        onTap: () {},
+                      ),
+                    ]),
+                ExpansionTile(
+                    title: const Text(
+                      'Marketplace',
+                      style: drawerMainMenuTextStyle,
+                    ),
+                    children: [
+                      ListTile(
+                        title: const Text('Dashboard'),
+                        leading: const Icon(Icons.dashboard),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Markets'),
+                        leading: const Icon(Icons.shopping_cart),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Products'),
+                        leading: const Icon(Icons.shopping_bag),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Product Catalogs'),
+                        leading: const Icon(Icons.list),
+                        onTap: () {},
+                      ),
+                      ListTile(
+                        title: const Text('Subscriptions'),
+                        leading: const Icon(Icons.subscriptions),
+                        onTap: () {},
+                      ),
+                    ]),
+                ExpansionTile(
+                    title: const Text(
+                      'Support',
+                      style: drawerMainMenuTextStyle,
+                    ),
+                    children: [
+                      ListTile(
+                        title: const Text('Tickets'),
+                        leading: const Icon(Icons.support_agent),
+                        onTap: () {},
+                      ),
+                    ]),
+                ListTile(
                   title: const Text(
-                    'IAM',
+                    'Partnership',
                     style: drawerMainMenuTextStyle,
                   ),
-                  children: [
-                    ListTile(
-                      title: const Text('Accounts'),
-                      leading: const Icon(Icons.account_circle),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Users'),
-                      leading: const Icon(Icons.people),
-                      onTap: () {},
-                    ),
-                  ]),
-              ExpansionTile(
-                  title: const Text(
-                    'CRM',
-                    style: drawerMainMenuTextStyle,
-                  ),
-                  children: [
-                    ListTile(
-                      title: const Text('Accounts'),
-                      leading: const Icon(Icons.account_circle),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('User'),
-                      leading: const Icon(Icons.people),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Accounts Managers'),
-                      leading: const Icon(Icons.manage_accounts),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Opportunities'),
-                      leading: const Icon(Icons.money),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Quotes'),
-                      leading: const Icon(Icons.request_quote),
-                      onTap: () {},
-                    ),
-                  ]),
-              ExpansionTile(
-                  title: const Text(
-                    'Marketplace',
-                    style: drawerMainMenuTextStyle,
-                  ),
-                  children: [
-                    ListTile(
-                      title: const Text('Dashboard'),
-                      leading: const Icon(Icons.dashboard),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Markets'),
-                      leading: const Icon(Icons.shopping_cart),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Products'),
-                      leading: const Icon(Icons.shopping_bag),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Product Catalogs'),
-                      leading: const Icon(Icons.list),
-                      onTap: () {},
-                    ),
-                    ListTile(
-                      title: const Text('Subscriptions'),
-                      leading: const Icon(Icons.subscriptions),
-                      onTap: () {},
-                    ),
-                  ]),
-              ExpansionTile(
-                  title: const Text(
-                    'Support',
-                    style: drawerMainMenuTextStyle,
-                  ),
-                  children: [
-                    ListTile(
-                      title: const Text('Tickets'),
-                      leading: const Icon(Icons.support_agent),
-                      onTap: () {},
-                    ),
-                  ]),
-              ListTile(
-                title: const Text(
-                  'Partnership',
-                  style: drawerMainMenuTextStyle,
+                  onTap: () {},
                 ),
-                onTap: () {},
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
